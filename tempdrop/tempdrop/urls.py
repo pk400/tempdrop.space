356f31001318ada62e9e94814dbbc8b8cb138526
Joel Aro <joelaro.to@gmail.com>
2020-07-10 04:22:31 -0400

    Removed static.

--- tempdrop/tempdrop/urls.py
+++ tempdrop/tempdrop/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.urls import re_path
-from django.conf import settings
-from django.conf.urls.static import static
 
 from server.views import index
 
@@ -12,4 +10,4 @@ urlpatterns = [
   # Ensure the above paths are resolved first before handling frontend urls.
   re_path(r'^$', index, name='index'),
   re_path(r'^(?:.*)/?$', index),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
