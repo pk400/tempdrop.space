43413af6eb70dbbf576d08fbcca87a3e55c6c391
Joel Aro <joelaro.to@gmail.com>
2020-07-16 00:12:29 -0400

    Updated Views to use s3boto3 storage.

--- tempdrop/server/urls.py
+++ tempdrop/server/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('file/', views.add_file, name='add_file'),
-  path('file/<str:share_id>', views.get_file, name='get_file')
+  path('file/', views.FilesView.as_view()),
+  path('file/<str:share_id>', views.FilesView.as_view(), name='get_file')
 ]
